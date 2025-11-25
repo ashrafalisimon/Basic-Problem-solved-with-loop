ca8d4384b3d42e0ee05f669f433d0a7e57799790
Ashraf Ali <ashrafalicitp@gmail.com>
2025-11-25 18:37:20 +0600

    maximum number print

---
+++ E_Max.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int mx = INT_MIN;
+    for(int i=1; i<=n; i++){
+        int val;
+        scanf("%d",&val);
+        if(val > mx){
+            mx = val;
+        }
+    }
+    printf("%d", mx);
+    return 0;
+}
